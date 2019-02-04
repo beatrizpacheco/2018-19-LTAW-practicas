2e0f75e4d77715570a9e6151308939e701fdc22a
Beatriz Pacheco Pinero <bpacheco@delta34.aulas.gsyc.urjc.es>
2019-02-04 12:57:19 +0100

    clase 4

--- Practica-1/web2/server.js
+++ Practica-1/web2/server.js
@@ -7,7 +7,22 @@ console.log("Arrancando servidor...")
 http.createServer(function (req, res) {
   fs.readFile('cv.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
+    console.log("---> Peticion recibida")
+    console.log("Recurso solicitado (URL): " + req.url)
+    if (req.url == '/'){
+      fs.readFile('req.url', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+      })
+    } else if (req.url.split('.')[1] == 'css'){
+      res.readHead(200, {'Content-Type': 'text/css'});
+      res.write(req.url.split('/')[1]);
+      return res.end();
+      //console.log(req.url);
+    } else if (req.url.split('.')[1] == 'js'){
+      fs.readFile('req.url', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+      })
+    }
+    res.end(data);
   });
 }).listen(8080);
