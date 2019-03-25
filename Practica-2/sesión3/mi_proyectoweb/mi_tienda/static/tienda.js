9b6109fee025ea112fb67026bea449d8c87e66fa
Beatriz Pacheco Pinero <bpacheco@alpha27.aulas.gsyc.urjc.es>
2019-03-25 17:43:04 +0100

    archivos borrados

--- Practica-2/sesión3/mi_proyectoweb/mi_tienda/static/tienda.js
+++
@@ -1,8 +0,0 @@
-function CargarJsonLocal(params) {
-    var request2 = new XMLHttpRequest();
-    request2.open("GET", "products.json", false);
-    request2.send()
-    console.log(request2.responseType);
-    var people2 = JSON.parse(request2.responseText);
-    console.log(people2);
-}
