0477bb9ecbc3ca99613027c09458fbc29bdc5a74
beatrizpacheco <beatriz.pacheco@hotmail.es>
2019-05-14 16:13:11 +0200

    inicio p5

---
+++ Practica-5/chat_client.js
@@ -0,0 +1,38 @@
+const io = require('socket.io-client');
+const socket = io('http://localhost:3000',{transport:['websocket']});
+
+function main()
+{
+  console.log("Probando...")
+
+  var socket = io();
+
+  //-- Obtener los elementos de interfaz:
+
+ //-- Boton de envio de mensaje
+ var send = document.getElementById('send')
+
+ //-- Parrafo para mostrar mensajes recibidos
+ var display = document.getElementById('display')
+
+ //-- Caja con el mensaje a enviar
+ var msg = document.getElementById("msg")
+
+ //-- Cuando se aprieta el botón de enviar...
+ send.onclick = () => {
+   console.log(msg.value);
+   //-- Enviar el mensaje, con el evento "new_message"
+   socket.emit('new_message', msg.value);
+   document.getElementById("msg").value = '';
+
+   //-- Lo notificamos en la consola del navegador
+   console.log("Mensaje emitido")
+ }
+
+ //-- Cuando se reciba un mensaje del servidor se muestra
+  //-- en el párrafo
+  socket.on('new_message', msg => {
+    display.innerHTML += msg + '<br>';
+  });
+
+}
